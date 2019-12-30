3100d609f38a50b96b327b0251737a1628b1a4a3
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2019-12-30 12:35:32 -0800

    Add my solution to excercise 2, chap 1

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -14,11 +14,42 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemResp(dct_file='2002FemResp.dct', dat_file='2002FemResp.dat.gz'):
+	dct = thinkstats2.ReadStataDct(dct_file) 
+	df = dct.ReadFixedWidth(dat_file, compression='gzip') 
+	CleanFemResp(df) 
+	return df
+
+
+def CleanFemResp(df):
+    pass
+
+
+def ValidatePregnum(resp_df, preg_df):
+	preg_map = nsfg.MakePregMap(preg_df)
+
+	for index, caseid in preg_df.caseid.iteritems(): 
+		pregnum = preg_df.pregnum[index]
+
+		if len(preg_map[caseid]) != pregnum:
+			return False 
+
+	return True
+
+
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+
+    resp_df = ReadFemResp()
+    preg_df = nsfg.ReadFemPreg()
+
+    assert(len(resp_df) == 7643)
+    assert(resp_df.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(resp_df, preg_df))
+
     print('%s: All tests passed.' % script)
 
 
